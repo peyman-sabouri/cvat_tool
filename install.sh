44e1658907cb124d6f1257c7d15140172b0fbbe2
Peyman <29961767+peyman-sabouri@users.noreply.github.com>
2019-07-15 15:16:43 +1200

    modify installer

--- install.sh
+++ install.sh
@@ -1,19 +1,12 @@
 sudo apt-get update
-sudo apt-get install -y \
-  apt-transport-https \
-  ca-certificates \
-  curl \
-  gnupg-agent \
-  software-properties-common
+sudo apt-get install -y   apt-transport-https   ca-certificates   curl   gnupg-agent   software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) \
-  stable"
+sudo add-apt-repository   "deb [arch=amd64] https://download.docker.com/linux/ubuntu   $(lsb_release -cs)   stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
-
+sudo apt-get install -y python3-pip
+sudo pip3 install docker-compose
